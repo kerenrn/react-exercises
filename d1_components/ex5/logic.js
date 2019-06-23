0bca5d39fd1d2dc8af6780b907bb48428d70daae
keren <kerenr@gmail.com>
2019-06-23 16:16:02 +0300

    day1 ex5

--- d1_components/ex5/logic.js
+++ d1_components/ex5/logic.js
@@ -1,4 +1,22 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Button/>
+            </div>
+        )
+    }
+}
+class Button extends React.Component {
+    render() {
+        return (
+            <button>Click</button>
+        );
+        
+    }
+}
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <App />,
     document.getElementById("root")
 );
