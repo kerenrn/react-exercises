932eee70578453ff6a876cac258daad69363a7ae
keren <kerenr@gmail.com>
2019-06-24 15:23:20 +0300

    day 2 star ex1

--- d2_JSX/ex4/logic.js
+++ d2_JSX/ex4/logic.js
@@ -1,4 +1,23 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+function render(data) {
+    ReactDOM.render(
+        <div>
+            <a src={data.linkAddress}>go to {data.linkText}</a>
+            <input id="slider" type="range" min={data.min - 1} max={data.max * 2} step="2" />
+            <button id="click" style={data}>click me</button>
+        </div>,
+        document.getElementById("root")
+    );
+
+
+}
+var data = {
+    linkAddress: "https://www.google.com/",
+    linkText: "google",
+    min: -5,
+    max: 5,
+    color: "maroon",
+    "font-weight": "bold",
+    buttonText: "click me"
+};
+render(data)
+
