793cdfbf0f4f52ed7c5a88c19aff706d14d03163
keren <kerenr@gmail.com>
2019-06-24 15:28:47 +0300

    fix to day 2 star ex1

--- d2_JSX/ex4/logic.js
+++ d2_JSX/ex4/logic.js
@@ -1,7 +1,7 @@
 function render(data) {
     ReactDOM.render(
         <div>
-            <a src={data.linkAddress}>go to {data.linkText}</a>
+            <a href={data.linkAddress} target="_blank">go to {data.linkText}</a>
             <input id="slider" type="range" min={data.min - 1} max={data.max * 2} step="2" />
             <button id="click" style={data}>click me</button>
         </div>,
