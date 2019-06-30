4aa543112999b77c866c854c44cfe6a681b90ca2
keren <kerenr@gmail.com>
2019-06-30 14:42:24 +0300

    star ex 2. Disable buttons

--- d4_state/ex4/logic.js
+++ d4_state/ex4/logic.js
@@ -1,4 +1,40 @@
+class ColorButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDisabled: false,
+        };
+        this.disableButton = this.disableButton.bind(this);
+    }
+
+    disableButton() {
+        let newState = this.state.isDisabled ? false : true;
+        this.setState({ isDisabled: newState });
+    }
+
+    render() {
+        let classname = this.state.isDisabled ? "disabled" : "";
+        return (
+            <button onClick={this.disableButton} className={classname}>{this.props.color}</button>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <ColorButton color="blue" />
+                <ColorButton color="purple" />
+                <ColorButton color="green" />
+                <ColorButton color="red" />
+                <ColorButton color="pink" />
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <App />,
     document.getElementById("root")
 );
