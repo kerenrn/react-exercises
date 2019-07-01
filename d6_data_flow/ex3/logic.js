2f1db6066e2b87bb13802e95ab08999c149e60d9
keren <kerenr@gmail.com>
2019-07-01 14:58:36 +0300

    day 5 satar ex 1. Buttons set the color of the box.

--- d6_data_flow/ex3/logic.js
+++ d6_data_flow/ex3/logic.js
@@ -1,4 +1,46 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            boxColor: "",
+        };
+        this.updateBoxColor = this.updateBoxColor.bind(this);
+    }
+
+    updateBoxColor(color) {
+        this.setState({ 
+            boxColor: color
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Button handleClick={this.updateBoxColor} text="Blue"/>
+                <Button handleClick={this.updateBoxColor} text="Green"/>
+                <Button handleClick={this.updateBoxColor} text="Orange"/>
+                <div className={this.state.boxColor}></div>
+            </div>
+        )
+    }
+}
+
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.sendColor = this.sendColor.bind(this);
+    }
+    sendColor(event) {
+        this.props.handleClick(event.target.textContent);
+    }
+    render() {
+        return (
+            <button onClick={this.sendColor}>{this.props.text}</button>
+        )
+    }
+}
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <App />,
     document.getElementById("root")
 );
